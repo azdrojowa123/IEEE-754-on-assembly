da1353bb9f6718e65689a18854cfe944e6aff23a
Aleksandra Zdrojowa <a.zdrojowa@gmail.com>
2020-05-19 19:24:56 +0200

    modify zad5

--- zad5c.c
+++ zad5c.c
@@ -1,22 +1,18 @@
 #include <stdio.h>
 
 void Basic2();
-void  Basic();
+void Basic();
 unsigned long long times();
 
-unsigned long long time1, time2;
 unsigned long after, before;
 int main(){
 
 	
-	time1= times();
+	
 	Basic();
-	time2=times();
-	printf("Czas wykonywania operacji powiązanych ze sobą: %llu\n",time2-time1);
-	time1=times();
+	printf("Czas wykonywania operacji nie  powiązanych ze sobą: %lu\n",after-before);
 	Basic2();
-	time2=times();
-	printf("Czas wykonywania operacji niezależnych od siebie: %llu\n",time2-time1);
+	printf("Czas wykonywania operacji powiązanych  ze sobą: %lu\n",after-before);
 
 	return 0;
 }
--- zad5s.s
+++ zad5s.s
@@ -3,65 +3,71 @@
 
 .section .text
 
-Basic: 
+.macro Timer
+xor %eax, %eax
+cpuid 
+rdtsc
+.endm
 
-# operacje powiazane ze soba zależnościamia
+#operacje nie powiązane ze sobą
+Basic:
+push %ebx
+push %esi
+ 
+
 FINIT
 FLD1
 FLDPI
 FLDL2T
 FLDLG2
-FADD %st(1), %st(0)
-FMUL %st(0), %st(1)
-FADD %st(2), %st(0)
-FMUL %st(0), %st(2)
-FADD %st(1), %st(0)
-FMUL %st(0), %st(1)
-FADD %st(3), %st(0)
-FMUL %st(0), %st(3)
+FLDPI
+
+Timer 
+movl %eax, %esi
+
+.rept 10
 FADD %st(0), %st(1)
-FMUL %st(2), %st(0)
-FADD %st(0), %st(2)
-FMUL %st(3), %st(0)
-FADD %st(3), %st(0)
-FMUL %st(0), %st(3)
-FADD %st(2), %st(0)
 FMUL %st(0), %st(2)
-FADD %st(1), %st(0)
-FMUL %st(0), %st(1)
-FADD %st(2), %st(0)
-FMUL %st(0), %st(2)
+FADD %st(0), %st(3)
+FMUL %st(0), %st(4)
+.endr
+
+Timer 
+movl %eax, after
+movl %esi, before
+
+pop %esi
+pop %ebx
 ret
 
 
 
-#operacje nie powiązane
+#operacje  powiązane
 Basic2: 
+push %ebx
+push %esi
 
 FINIT
 FLD1
 FLDPI 
 FLDL2T
 FLDLG2
+
+Timer 
+movl %eax, %esi
+
+.rept 10
 FADD %st(1), %st(0)
 FMUL %st(2), %st(0)
 FADD %st(3), %st(0)
-FMUL %st(1), %st(0)
-FADD %st(2), %st(0)
-FMUL %st(3), %st(0)
-FADD %st(1), %st(0)
-FMUL %st(2), %st(0)
-FADD %st(3), %st(0)
-FMUL %st(1), %st(0)
-FADD %st(2), %st(0)
-FMUL %st(3), %st(0)
-FADD %st(1), %st(0)
-FMUL %st(2), %st(0)
-FADD %st(3), %st(0)
-FADD %st(1), %st(0)
-FMUL %st(2), %st(0)
-FMUL %st(3), %st(0)
-FADD %st(1), %st(0)
-FMUL %st(2), %st(0)
+FMUL %st(4), %st(0)
+.endr
+
+Timer
+movl %eax, after
+movl %esi, before
+
+pop %esi
+pop %ebx
 
 ret
